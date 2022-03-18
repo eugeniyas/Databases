33027e9ab788b7ab32c2209c6e35b881ecc4fe61
eugeniyas <63968897+eugeniyas@users.noreply.github.com>
2022-03-19 01:03:36 +0300

    Add files via upload

---
+++ L26HW13/Script.sql
@@ -0,0 +1,35 @@
+
+-- максимальная и минимальная цена товара и кол-во предложений
+SELECT 	MIN(price), 
+		MAX(price), 
+		COUNT(*) 
+FROM 	store.product;
+
+-- самый дорогой и самый дешевый товар в каждой категории
+SELECT 	pc.category_id, 
+		MIN(p.price), 
+		MAX(p.price)
+FROM 	store.product p 
+		INNER JOIN store.product_category pc ON pc.product_id  = p.product_id
+GROUP BY pc.category_id;
+
+-- rollup с количеством товаров по категориям
+SELECT 	IF(GROUPING(pc.category_id), 'All', pc.category_id) AS category, 
+		COUNT(*)
+FROM 	store.product p 
+		INNER JOIN store.product_category pc ON pc.product_id  = p.product_id
+GROUP BY pc.category_id WITH ROLLUP;
+
+-- самый дорогой и самый дешевый товар в каждой категории, в которой насчитывается более 10 товаров
+SELECT 	pc.category_id,
+		MIN(CASE 
+				WHEN p.price < 1 THEN 0 
+				ELSE p.price
+			END) AS MinPrice, 
+		MAX(p.price) AS MaxPrice, 
+		COUNT(*) AS Count
+FROM 	store.product p 
+		INNER JOIN store.product_category pc ON pc.product_id  = p.product_id
+GROUP BY pc.category_id 
+HAVING COUNT(*) > 1;
+
