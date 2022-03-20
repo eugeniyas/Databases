9ce9fb0e7e4a46568b40a40ae517733f5631e7fb
eugeniyas <63968897+eugeniyas@users.noreply.github.com>
2022-03-20 23:00:01 +0300

    Update Indexes.sql

--- L27HW14/Indexes.sql
+++ L27HW14/Indexes.sql
@@ -3,4 +3,4 @@ CREATE FULLTEXT INDEX product_fulltext_idx ON product (title, description);
 EXPLAIN
 SELECT 	* 
 FROM 	product 
-WHERE 	MATCH (title, description) AGAINST ('неавтоматическая');
+WHERE 	MATCH (title, description) AGAINST ('РЅРµР°РІС‚РѕРјР°С‚РёС‡РµСЃРєР°СЏ');
